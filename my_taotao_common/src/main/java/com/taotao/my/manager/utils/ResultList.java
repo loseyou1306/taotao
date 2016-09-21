72e62107662813913f375893da4d2f6f6184f706
liliang <1411889141@qq.com>
2016-09-21 21:43:50 +0800

    my_taotao_commom

---
+++ my_taotao_common/src/main/java/com/taotao/my/manager/utils/ResultList.java
@@ -0,0 +1,22 @@
+package com.taotao.my.manager.utils;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ResultList implements Serializable {
+	private Integer total;
+	private List<?> rows;
+	public Integer getTotal() {
+		return total;
+	}
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+	public List<?> getRows() {
+		return rows;
+	}
+	public void setRows(List<?> rows) {
+		this.rows = rows;
+	}
+	
+}
