9b086d5c4764e23a93cc7068795124d5371fa5ff
liliang <1411889141@qq.com>
2016-09-25 19:58:52 +0800

    门户系统web层项目

---
+++ my_taotao_portal_web/src/main/java/com/taotao/my/portal/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.taotao.my.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping("/index")
+	public String toIndex(){
+		return "index";
+	}
+}
