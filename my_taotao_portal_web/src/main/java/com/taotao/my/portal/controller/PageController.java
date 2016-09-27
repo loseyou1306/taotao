c25fcc5609554f392114003cd6de58b83774c2b9
liliang <1411889141@qq.com>
2016-09-27 10:11:56 +0800

    修改门户网站页面大广告位置处的数据

--- my_taotao_portal_web/src/main/java/com/taotao/my/portal/controller/PageController.java
+++ my_taotao_portal_web/src/main/java/com/taotao/my/portal/controller/PageController.java
@@ -1,13 +1,32 @@
 package com.taotao.my.portal.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.taotao.my.content.service.ContentService;
+import com.taotao.my.manager.utils.AdItem;
+import com.taotao.my.manager.utils.JsonUtils;
 
 @Controller
 public class PageController {
-	
+	@Value("${AD_CATEGORYID}")
+	private Long AD_CATEGORYID;
+	@Autowired
+	private ContentService contentService;
+	/**
+	 * 查询大广告位置处的信息
+	 * 查询参数:Long categoryId
+	 * 返回值(json数据):List<AdItem>
+	 */
 	@RequestMapping("/index")
-	public String toIndex(){
+	public String toIndex(Model model){
+		List<AdItem> list = contentService.findContentListByCategoryId(AD_CATEGORYID);
+		model.addAttribute("ad1", JsonUtils.objectToJson(list));
 		return "index";
 	}
 }
